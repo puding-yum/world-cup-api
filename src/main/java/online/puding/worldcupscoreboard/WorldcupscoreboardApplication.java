2cdd6a9a7c051966b28023a89ef9dbfc9af4dc9b
puding-yum <hamid1bae1@gmail.com>
2026-07-13 10:18:16 +0700

    init commit

---
+++ src/main/java/online/puding/worldcupscoreboard/WorldcupscoreboardApplication.java
@@ -0,0 +1,13 @@
+package online.puding.worldcupscoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldcupscoreboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldcupscoreboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/puding/worldcupscoreboard/WorldcupscoreboardApplicationTests.java
@@ -0,0 +1,13 @@
+package online.puding.worldcupscoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldcupscoreboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
